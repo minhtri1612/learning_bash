de0f2acb32d19802796f7352eb67156f83ecba36
minhtri1612 <10422119@vgu.edu.vn>
2025-11-10 23:16:46 +0700

    add case script in logic

---
+++ logic/case_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Please enter a number: " number
+
+case "$number" in
+        [0-9]) echo "you have entered a single digit number";;
+        [0-9][0-9]) echo "you have entered a two digit number";;
+        [0-9][0-9][0-9]) echo "you have entered a three digit number";;
+        *) echo "you have entered a number that is more than 3 digits";;
+esac
